7d3e6f6e088ec1535d84f96b3618f26f2079df05
Jonas Thelemann <e-mail@jonas-thelemann.de>
2022-11-27 01:08:19 +0100

    feat: upgrade to dargstack v2

--- src/development/certificates/mkcert.sh
+++ src/development/certificates/mkcert.sh
@@ -1,10 +1,53 @@
 #!/bin/sh
-THIS=$(dirname $(readlink -f "$0"))
-NAME=jonas-thelemann
+THIS=$(dirname "$(readlink -f "$0")")
 
-mkcert \
--cert-file "$THIS/$NAME.crt" \
--ecdsa \
--key-file "$THIS/$NAME.key" \
-    $NAME.test \
-    "*.$NAME.test"
+create() {
+    NAME="$1"
+    shift
+    CONTENT=$*
+
+    path="$THIS/$NAME"
+    certfile="$path.crt"
+    keyfile="$path.key"
+
+    if [ "$CONTENT" != "" ]; then
+        # shellcheck disable=SC2086
+        mkcert \
+            -cert-file "$certfile" \
+            -ecdsa \
+            -key-file "$keyfile" $CONTENT
+    fi
+
+    cat "$(mkcert -CAROOT)/rootCA.pem" >> "$certfile"
+}
+
+rm "$THIS"/*.key "$THIS"/*.crt
+
+create "root"
+create "traefik" \
+    # 1generator
+    "1generator.localhost" \
+    # adminer
+    "adminer.localhost" \
+    # creal
+    "creal.localhost" \
+    # creal/postgraphile
+    "creal-postgraphile.localhost" \
+    # creal/strapi
+    "strapi.localhost" \
+    # hedgedoc
+    "hedgedoc.localhost" \
+    # jonas-thelemann
+    "localhost" "www.localhost" "127.0.0.1" "0.0.0.0" \
+    # nextcloud
+    "nextcloud.localhost" \
+    # portainer
+    "portainer.localhost" \
+    # thelounge
+    "thelounge.localhost" \
+    # traefik
+    "traefik.localhost" \
+    # trapparty
+    "trapparty.localhost" \
+    # trapparty/postgraphile
+    "trapparty-postgraphile.localhost"
